2cd80ce9f11d023f638779a3786ec6a4333bbab5
evan-smart <esmart029@gmail.com>
2026-07-19 02:13:04 -0700

    Create initial Next.js frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>
+        ScreenScout 🎬
+      </h1>
+
+      <p>
+        Find the best movie theater experience near you.
+      </p>
+    </main>
+  );
+}
